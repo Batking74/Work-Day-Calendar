e649896df38307cced9bc6aa87f1c2059d4ad004
Nazir Knuckles <nazirknuckles@gmail.com>
2023-10-31 00:46:52 -0400

    Made changes to the index.html and JS file. I fixed a few logic errors in js file that was affecting the save buttons and colors on the webpage

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,54 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+$(function() {
+  var today = dayjs();
+  displayPastText();
+  setSaveBtnsEventListener();
+
   // TODO: Add code to display the current date in the header of the page.
+  $('#currentDay').text(today.format('dddd, MMMM DD'));
+  
+  const date = new Date($.now());
+  const currentHour = date.getHours();
+
+  for (let i = 9; i <= 17; i++) {
+    const element = $(`#hour-${i}`);
+
+    if (i < currentHour) {
+      element[0].classList.add('past');
+    }
+    else if (i == currentHour) {
+      element[0].classList.add('present');
+    }
+    else {
+      element[0].classList.add('future');
+    }
+  }
 });
+
+// Displays saved past text from localstorage for each textarea once user clicks save.
+function displayPastText() {
+  $('.row').each(function() {
+    var element = $(this);
+    var id = element.attr('id');
+    var text = element.children('textarea');
+    var pastText = localStorage.getItem(id);
+    text.val(pastText);
+  })
+}
+
+// Sets event listeners for all save btns, and if clicked saves to localstorage.
+function setSaveBtnsEventListener() {
+  $('.btn').click(function(e) {
+    let textArea;
+
+    if (e.target.parentElement.classList[0] === 'btn') {
+      textArea = e.target.parentElement.previousElementSibling;
+    }
+    else {
+      textArea = e.target.parentElement.children[1];
+    }
+
+    var id = textArea.parentElement.id;
+    var previousElementValue = textArea.value;
+    localStorage.setItem(id, previousElementValue);
+  })
+}
